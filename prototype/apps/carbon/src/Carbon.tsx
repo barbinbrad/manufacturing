de1ae236a143820750aa425bec8bf0ce3c4c696f
Brad Barbin <bradbarbin@protonmail.com>
2022-05-02 15:18:37 -0400

    merge from main

--- prototype/apps/carbon/src/Carbon.tsx
+++ prototype/apps/carbon/src/Carbon.tsx
@@ -3,28 +3,18 @@ import { Routes, Route } from 'react-router-dom';
 import CatchError from './components/CatchError';
 import AppLauncher from './AppLauncher';
 import Base from './Base';
-<<<<<<< HEAD
 import Explorer from './Explorer';
 import Module from './Module';
 
 export type Props = {};
 
-=======
-import Flow from './Flow';
-import Module from './Module';
-
->>>>>>> main
 const Carbon: React.FC<Props> = props => {
   return (
     <CatchError>
       <Routes>
         <Route path="/" element={<Base />}>
           <Route index element={<AppLauncher />} />
-<<<<<<< HEAD
           <Route path="module" element={<Explorer />}>
-=======
-          <Route path="module" element={<Flow />}>
->>>>>>> main
             <Route path=":moduleId" element={<Module />} />
             {/* <Route path="new" element={<NewModule />} />
             <Route index element={<ModuleExplorer />} /> */}
@@ -35,11 +25,4 @@ const Carbon: React.FC<Props> = props => {
   )
 };
 
-<<<<<<< HEAD
-=======
-export type Props = {
-  
-}
-
->>>>>>> main
 export default Carbon;
