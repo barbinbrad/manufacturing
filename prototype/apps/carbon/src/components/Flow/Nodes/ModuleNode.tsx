4c4d880276dee4092f4687b6d4a20627a3df18e5
Brad Barbin <bradbarbin@protonmail.com>
2022-05-02 22:03:26 -0400

    fix merge conflicts

--- prototype/apps/carbon/src/components/Flow/Nodes/ModuleNode.tsx
+++ prototype/apps/carbon/src/components/Flow/Nodes/ModuleNode.tsx
@@ -1,10 +1,6 @@
 import * as React from 'react';
 import { Handle, Position } from 'react-flow-renderer';
-<<<<<<< HEAD:prototype/apps/carbon/src/components/Flow/Nodes/ModuleNode.tsx
 import Node from './Node';
-=======
-import styled from 'styled-components';
->>>>>>> main:prototype/apps/carbon/src/components/Nodes/ModuleNode.tsx
 
 type Props = {
   data: Data;
@@ -29,15 +25,9 @@ export default React.memo(({ data, isConnectable } : Props) => {
         onConnect={(params: any) => console.log('handle onConnect', params)}
         isConnectable={isConnectable}
       />
-<<<<<<< HEAD:prototype/apps/carbon/src/components/Flow/Nodes/ModuleNode.tsx
       <Node type="module" title="Module" >
         {data.title}
       </Node>
-=======
-      <Module >
-        {data.title}
-      </Module>
->>>>>>> main:prototype/apps/carbon/src/components/Nodes/ModuleNode.tsx
       
       <Handle
         type="source"
@@ -49,11 +39,3 @@ export default React.memo(({ data, isConnectable } : Props) => {
     </>
   );
 });
-<<<<<<< HEAD:prototype/apps/carbon/src/components/Flow/Nodes/ModuleNode.tsx
-=======
-
-const Module = styled.div`
-  background: #ff0000;
-  padding: 1rem;
-`
->>>>>>> main:prototype/apps/carbon/src/components/Nodes/ModuleNode.tsx
